64a0532c4a412ef36f8220c642d410058b329b7c
Muhammad Urwatil Wutsqo <60605671+wutsqo@users.noreply.github.com>
2024-05-08 14:27:58 +0700

    Update settings.py

--- core/settings.py
+++ core/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = ["127.0.0.1", "localhost", ".vercel.app"]
+ALLOWED_HOSTS = ["127.0.0.1", "localhost", ".vercel.app", "api.law.wutzz.space"]
 
 
 # Application definition
